e3b651da3a60968040e86e6a33becbef5b29a93d
Łukasz Walaszczyk <l.walaszczyk@interia.pl>
2020-09-07 00:57:10 +0200

    Updates

---
+++ precache-manifest.ddf9c57ef451482e82d0871752bd3e86.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9006c5282aff88c032f2eb9585309f6c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b41d1e9ec04bc5aa5be7",
+    "url": "/portfolio/static/css/2.1966d606.chunk.css"
+  },
+  {
+    "revision": "b2033f3a358f36dc6b51",
+    "url": "/portfolio/static/css/main.e1cb03dc.chunk.css"
+  },
+  {
+    "revision": "b41d1e9ec04bc5aa5be7",
+    "url": "/portfolio/static/js/2.c157e3fe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.c157e3fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2033f3a358f36dc6b51",
+    "url": "/portfolio/static/js/main.b5be22e1.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
