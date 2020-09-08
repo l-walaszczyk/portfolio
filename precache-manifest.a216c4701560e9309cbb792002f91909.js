ff786048b8de681bdc6319be5e7647ee54de2e58
Łukasz Walaszczyk <l.walaszczyk@interia.pl>
2020-09-08 13:25:51 +0200

    Updates

---
+++ precache-manifest.a216c4701560e9309cbb792002f91909.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee3b1a0d8fed29aef937c41bbcef4d1d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "416e911992ff04161200",
+    "url": "/portfolio/static/css/2.1966d606.chunk.css"
+  },
+  {
+    "revision": "f9dfa4a0fe8d2fd533c0",
+    "url": "/portfolio/static/css/main.927932eb.chunk.css"
+  },
+  {
+    "revision": "416e911992ff04161200",
+    "url": "/portfolio/static/js/2.50f37444.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.50f37444.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9dfa4a0fe8d2fd533c0",
+    "url": "/portfolio/static/js/main.54ac43c0.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "90755f2c0c541db32b4efecaca3a42bb",
+    "url": "/portfolio/static/media/html.90755f2c.svg"
+  },
+  {
+    "revision": "55e9876a01e7bc2f9db803a877043b1d",
+    "url": "/portfolio/static/media/interests.55e9876a.jpg"
+  },
+  {
+    "revision": "0fe02b3739dc3b5ab4796c3869236706",
+    "url": "/portfolio/static/media/js.0fe02b37.svg"
+  },
+  {
+    "revision": "50f574265e6f5e0a032db22808e3520b",
+    "url": "/portfolio/static/media/past.50f57426.jpg"
+  },
+  {
+    "revision": "eb7c714da612500b0a14b87b2052b21a",
+    "url": "/portfolio/static/media/present.eb7c714d.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.f8f0d4b9d2f53c33022ac4f4f15f27d1.js"
+  "/portfolio/precache-manifest.a216c4701560e9309cbb792002f91909.js"
 );
 
 self.addEventListener('message', (event) => {
